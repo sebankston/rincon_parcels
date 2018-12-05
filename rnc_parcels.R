087dcc0f8fd70f7a9ebe25994f35954771a80f35
Sam Bankston <sebankston@gmail.com>
2018-12-05 07:49:17 -0800

    map of rincon pass 2 barriers and parcels

--- rnc_parcels.R
+++ rnc_parcels.R
@@ -6,14 +6,16 @@ library(googlesheets)
 library(janitor)
 library(htmlwidgets)
 
-rnc_parcels <- st_read(layer = "ParcelDetail4c4f0282d30c4b80a28f912df10e38ed", dsn = ".") %>% st_transform(crs = 4326)
+rnc_parcels <- st_read(layer = "ParcelDetail4c4f0282d30c4b80a28f912df10e38ed", dsn = ".") %>% st_transform(crs = 4326) %>% clean_names()
 
 rnc_deets <- read_csv("rincon_detailed_assessments.csv")
 
 leaflet() %>% 
   addProviderTiles("Esri.WorldStreetMap", group = "Street Map") %>% 
-  addProviderTiles("Esri.WorldImagery",group = "World Imagery") %>% 
-  addCircleMarkers(data = rnc_r2, label = rnc_r2$barrier_id, popup = ~paste0(rnc_r2$barrier_id, "<br/>", rnc_r2$pad_id), color = "red", group = "Detailed Required", radius = 5, fillOpacity = .5) %>% 
-  addCircleMarkers(data = rnc_nr, label = rnc_nr$barrier_id, popup = ~paste0(rnc_nr$barrier_id, "<br/>", rnc_nr$pad_id), color = "dodgerblue", group = "Non-Road Barrier", radius = 3, fillOpacity = 1) %>%
-  addCircleMarkers(data = rnc_r, label = rnc_r$barrier_id, popup = ~paste0(rnc_r$barrier_id, "<br/>", rnc_r$pad_id), color = "green", group = "Road Barrier", radius = 3, fillOpacity = 1) %>%
-  addLayersControl(baseGroups = c("Street Map", "World Imagery"), overlayGroups = c("Non-Road Barrier", "Road Barrier", "Detailed Required"))
+  addProviderTiles("Esri.WorldImagery",group = "World Imagery") %>%
+  addPolygons(data = rnc_parcels, weight = 1.5, color = "lawngreen", label = ~paste0("Owner: ", owner_9b), highlight = highlightOptions(weight = 3,color = "red",bringToFront = TRUE), group = "Parcels") %>% 
+  addCircleMarkers(data = rnc_deets, label = rnc_deets$barrier_id, popup = ~paste0(rnc_deets$barrier_id, "<br />", rnc_deets$PAD_id), radius = 5, color = "red", fillOpacity = 1, group = "Barriers") %>% 
+  addLayersControl(baseGroups = c("Street Map", "World Imagery"), overlayGroups = c("Parcels", "Barriers"))
+
+
+  
